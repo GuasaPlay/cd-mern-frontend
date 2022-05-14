4f06875c4ec7fb104c5321ff594265ee3c9533f5
Oscar Calle <gameplaysoscar@gmail.com>
2022-05-13 19:45:02 -0500

    Administrador de Producto (Parte III)

--- src/pages/EditProductPage.jsx
+++ src/pages/EditProductPage.jsx
@@ -29,7 +29,7 @@ export const EditProductPage = () => {
               ...initialValuesProduct,
               ...product,
             }}
-            action="edit"
+            action="update"
           />
         )}
       </section>
